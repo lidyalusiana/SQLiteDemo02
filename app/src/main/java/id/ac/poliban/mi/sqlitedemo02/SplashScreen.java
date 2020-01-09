7230848911ffa24e71a6e7e558008a47bb71255a
lidyalusiana <lidyalusiana15@gmail.com>
2020-01-10 01:41:03 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/sqlitedemo02/SplashScreen.java
@@ -0,0 +1,19 @@
+package id.ac.poliban.mi.sqlitedemo02;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(() ->{
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        },3000);
+    }
+}
